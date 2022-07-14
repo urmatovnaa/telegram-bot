be6944906ef126b95d7e9c79a124b65140821e05
Altynai <urmatovnaa05@gmail.com>
2022-07-14 15:56:32 +0600

    my first bot

---
+++ main2.py
@@ -0,0 +1,40 @@
+import telebot, wikipedia, re
+
+bot = telebot.TeleBot('5534599787:AAHlUYLdSTq2WRMURRPc75TrMwLoqO8feB8')
+
+wikipedia.set_lang('ru')
+
+
+def getwiki(s):
+    try:
+        ny = wikipedia.page(s)
+        wikitext = ny.content[:1000]
+        wikimas = wikitext.split('.')
+        wikimas = wikimas[:-1]
+        wikitext2 = ''
+        for x in wikimas:
+            if not('==' in x):
+                if(len((x.strip()))>3):
+                    wikitext2=wikitext2 + x + '.'
+            else:
+                break
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
+        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
+
+        return wikitext2
+
+    except Exception as e:
+        return 'В Wiki нет инфы по данному слов'
+
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, getwiki(message.text))
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
